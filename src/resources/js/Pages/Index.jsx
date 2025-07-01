2af59bdf537f9701e9699ec194be55b40740f869
ryoooosk <pond.well.0205@gmail.com>
2025-07-01 23:28:26 +0900

    chore(Index.jsx): tailwindcssの動作確認

--- src/resources/js/Pages/Index.jsx
+++ src/resources/js/Pages/Index.jsx
@@ -1,3 +1,15 @@
 export default function Index() {
-    return <h1>Hello World</h1>;
+    return (
+        <div className="min-h-dvh h-full flex flex-col">
+            <header className="h-13 w-full px-2 flex items-center border-b border-slate-800">
+                <h1 className="text-xl font-semibold tracking-wide text-slate-800">
+                    Inertia.js Demo
+                </h1>
+            </header>
+
+            <div className="flex-1 px-2 my-3">
+                <p>Hello World</p>
+            </div>
+        </div>
+    );
 }
