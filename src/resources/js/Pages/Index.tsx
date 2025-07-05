e19544e5dc35b1ca6d4f77ad57835322d7d1ab9f
ryoooosk <pond.well.0205@gmail.com>
2025-07-05 21:31:59 +0900

    chore: lintとformatを実行

--- src/resources/js/Pages/Index.tsx
+++ src/resources/js/Pages/Index.tsx
@@ -1,24 +1,24 @@
 type Truss = {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 };
 
 export default function Index() {
-    const test: Truss = {
-        id: 1,
-        name: "Test Truss",
-    };
-    return (
-        <div className="min-h-dvh h-full flex flex-col">
-            <header className="h-13 w-full px-2 flex items-center border-b border-slate-800">
-                <h1 className="text-xl font-semibold tracking-wide text-slate-800">
-                    Inertia.js Demo
-                </h1>
-            </header>
+  const test: Truss = {
+    id: 1,
+    name: 'Test Truss',
+  };
+  return (
+    <div className="min-h-dvh h-full flex flex-col">
+      <header className="h-13 w-full px-2 flex items-center border-b border-slate-800">
+        <h1 className="text-xl font-semibold tracking-wide text-slate-800">
+          Inertia.js Demo
+        </h1>
+      </header>
 
-            <div className="flex-1 px-2 my-3">
-                <p>Hello World</p>
-            </div>
-        </div>
-    );
+      <div className="flex-1 px-2 my-3">
+        <p>Hello World</p>
+      </div>
+    </div>
+  );
 }
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.tsx"],
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-    },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
+      refresh: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 });
