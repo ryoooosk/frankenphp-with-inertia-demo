3b062076daebfff4ecbb3f24085d073a1d9f7b47
ryoooosk <pond.well.0205@gmail.com>
2025-07-05 23:27:37 +0900

    refactor: phpstan適用による修正

--- src/app/Http/Controllers/IndexController.php
+++ src/app/Http/Controllers/IndexController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use Inertia\Response;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
         return Inertia::render('Index');
     }
