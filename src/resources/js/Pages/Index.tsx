7fa7f9fe1dbcf741a03c363cbfffefc6e47dd507
ryoooosk <pond.well.0205@gmail.com>
2026-03-26 23:14:35 +0900

    feat: ヘッダーをトップレベルのlayoutコンポーネントに

--- src/resources/js/Pages/Index.tsx
+++ src/resources/js/Pages/Index.tsx
@@ -1,15 +1,7 @@
 export default function Index() {
   return (
-    <div className="min-h-dvh h-full flex flex-col">
-      <header className="h-13 w-full px-2 flex items-center border-b border-slate-800">
-        <h1 className="text-xl font-semibold tracking-wide text-slate-800">
-          Inertia.js Demo
-        </h1>
-      </header>
-
-      <div className="flex-1 px-2 my-3">
-        <p>Hello World</p>
-      </div>
+    <div className="px-2 my-3">
+      <p>Hello World</p>
     </div>
   );
 }
