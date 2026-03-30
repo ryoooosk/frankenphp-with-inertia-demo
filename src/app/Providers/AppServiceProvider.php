61a7abf8c8ddb98561f709df0e2fb146e71724cb
ryosuke ikeido <ryosuke.iikd@gmail.com>
2026-03-30 17:28:41 +0900

    chore: OAuth2認可画面をinertiaで

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,22 @@ class AppServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(now()->addDays(15));
         Passport::refreshTokensExpireIn(now()->addDays(30));
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        // OAuth2認可同意画面をInertiaページでレンダリング
+        Passport::authorizationView(function (array $params) {
+            /** @var array<int, \Laravel\Passport\Scope> $scopes */
+            $scopes = $params['scopes'];
+
+            return Inertia::render('Auth/Authorize', [
+                'client' => [
+                    'name' => $params['client']->name,
+                ],
+                'scopes' => array_map(fn (\Laravel\Passport\Scope $scope) => [
+                    'id' => $scope->id,
+                    'description' => $scope->description,
+                ], $scopes),
+                'authToken' => $params['authToken'],
+            ])->toResponse(request());
+        });
     }
 }
