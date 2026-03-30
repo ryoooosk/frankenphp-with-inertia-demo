f5eac9f39182e6db0aaf79e23e6c70892fcfb2ef
ryosuke ikeido <ryosuke.iikd@gmail.com>
2026-03-30 17:05:52 +0900

    chore: userモデルをOAuth対応

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -9,12 +9,15 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\Contracts\OAuthenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements OAuthenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory;
     use Notifiable;
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
