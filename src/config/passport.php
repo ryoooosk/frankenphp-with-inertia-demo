544e9a29db525c5abf5f61a8cbdd1b7f016a8de1
ryoooosk <pond.well.0205@gmail.com>
2026-06-14 22:03:04 +0900

    chore(passport): 日本語説明を併記

--- src/config/passport.php
+++ src/config/passport.php
@@ -11,6 +11,10 @@ return [
     | authenticating users. This value should correspond with one of your
     | guards that is already present in your "auth" configuration file.
     |
+    | Passport がユーザーを認証する際に使用する認証ガードを指定します。
+    | この値は "auth" 設定ファイルに定義済みのガードのいずれかと
+    | 一致している必要があります。
+    |
     */
 
     'guard' => 'web',
@@ -26,6 +30,10 @@ return [
     | your application. By default, the keys are stored as local files but
     | can be set via environment variables when that is more convenient.
     |
+    | Passport は安全なアクセストークンを生成する際に暗号化キーを使用します。
+    | デフォルトではキーはローカルファイルとして保存されますが、その方が
+    | 都合の良い場合は環境変数を介して設定することもできます。
+    |
     */
 
     'private_key' => env('PASSPORT_PRIVATE_KEY'),
@@ -40,6 +48,10 @@ return [
     | By default, Passport's models will utilize your application's default
     | database connection. If you wish to use a different connection you
     | may specify the configured name of the database connection here.
+    |
+    | デフォルトでは、Passport のモデルはアプリケーションのデフォルトの
+    | データベース接続を使用します。別の接続を使用したい場合は、設定済みの
+    | データベース接続名をここで指定できます。
     |
     */
 
